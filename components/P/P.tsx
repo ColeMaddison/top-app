0c02d886ae14e74d04c7be7257691188c6364a23
ColeMaddison <dimonas92@gmail.com>
2022-06-20 13:38:34 +0200

    add p component

---
+++ components/P/P.tsx
@@ -0,0 +1,19 @@
+import * as styles from './P.module.css';
+import { PProps } from './P.props';
+import cn from 'classnames';
+
+export const P = ({ children, size = 'm', className, ...props }: PProps): JSX.Element => {
+    return (
+        <p
+            {...props}
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            className={cn(styles.p, className, {
+                [styles.s]: size === 's',
+                [styles.m]: size === 'm',
+                [styles.l]: size === 'l',
+            })}
+        >
+            {children}
+        </p>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "../components";
 import { Htag } from "../components/Htag/Htag";
+import { P } from "../components/P/P";
 
 export default function Home(): JSX.Element {
 	return (
@@ -8,6 +9,9 @@ export default function Home(): JSX.Element {
 			<Htag tag='h1' >Текст</Htag>
 			<Button appearance='primary' arrow='right'>hasas</Button>
 			<Button appearance='ghost' arrow='down'>hasas</Button>
+			<P size='l'>sdfsdfd</P>
+			<P>sdfsdfd</P>
+			<P size='s'>sdfsdfd</P>
 		</>
 	);
 }
